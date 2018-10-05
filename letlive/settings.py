26e296fdb244e2c98e9d73f48ac81e85de74219b
Kennedy M. Kioko <kmkioko@gmail.com>
2018-10-05 23:21:03 +0300

    Renamed '/Home ' folder and clean up code
    Renamed the folder  '/Home' to '/home' and cleaned up code by removing all python compiled code *.pyc

--- letlive/settings.py
+++ letlive/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Home',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
