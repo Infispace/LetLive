257593d39f1c128873fa63defc4148c1f10b6951
Kennedy M. Kioko <kmkioko@gmail.com>
2018-10-11 22:13:35 +0300

    Views Edited, ArticleView and TopicView
    Edited the above views  and their templates to build a skeleton

--- home/views.py
+++ home/views.py
@@ -10,7 +10,7 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Topic, Article
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm, ArticleForm
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -21,43 +21,63 @@ class IndexView(generic.ListView):
     #def get_queryset(self):
         #Article.objects.all()
 
-class TopicView(generic.DetailView):
-    model = Topic
+class TopicView(generic.TemplateView):
     template_name = 'home/topic.html'
+    topic = None
+    topic_list = None
 
     def get(self, request, topic_id=0):
         if topic_id > 0:
-            return HttpResponse('topic ' +str(topic_id))
+            self.topic = get_object_or_404(Topic, pk=topic_id)
         else:
-            return HttpResponse('topic view'+
-                reverse('home:topic_default')
-            )
+            self.topic_list = Topic.objects.all()
 
-class ArticleView(generic.DetailView):
-    model = Article
+        return render(request, self.template_name, {
+            'topic': self.topic, 'topic_list': self.topic_list
+        })
+
+class ArticleView(generic.TemplateView):
     template_name = 'home/article.html'
+    form = None
+    article_to_edit = None
+    article_list = None
 
-    def get(self, request, article_id=0):
-        if article_id > 0:
-            return HttpResponse('article ' +str(article_id))
+    def get(self, request, article_id=0, page='article'):
+        if page is 'article_new':
+            self.form = ArticleForm()
+        elif page is 'article_edit':
+            article_to_edit =  get_object_or_404(Article, pk=article_id)
+            self.form = ArticleForm(instance=article_to_edit)
         else:
-            return HttpResponse('article view')
+            if article_id > 0:
+                self.article_to_edit =  get_object_or_404(Article, pk=article_id)
+            else:
+                # render some articles in my blog
+                self.article_list = Article.objects.all()
 
+        return render(request, self.template_name, {
+            'page': page,
+            'form': self.form,
+            'article': self.article_to_edit,
+            'blog_article_list': self.article_list
+        })
 
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
         pass
 
-class UserView(generic.DetailView):
-    model = User
+class UserView(generic.TemplateView):
     template_name = 'home/user.html'
+    view_user = None
 
     @method_decorator(login_required)
     def get(self, request, user_id=0):
         if user_id > 0:
-            return HttpResponse('user view '+str(user_id))
-        else:
-            return HttpResponse('user view '+request.user.username)
+            self.view_user = get_object_or_404(User, pk=user_id)
+
+        return render(request, self.template_name,
+            {'next': next, 'view_user': self.view_user}
+        )
 
     @login_required()
     def userLogout(request):
@@ -69,39 +89,57 @@ class UserView(generic.DetailView):
 
 class UserLoginView(generic.TemplateView):
     template_name = 'home/login.html'
-    form = LoginForm()
+    form = None
+    error_string = ''
 
-    def get(self, request, next=''):
+    def get(self, request, page='login', next=''):
         if 'next' in request.GET:
             next = request.GET['next']
 
-        return render(request, self.template_name,
-            {'next': next, 'form': self.form}
-        )
-
-    def post(self, request, next='', error='', user=None):
-        if 'next' in request.POST:
-            next = request.POST['next']
-
-        self.form = LoginForm(request.POST)
-        if self.form.is_valid():
-            user = authenticate(username=self.form.cleaned_data['username'],
-                password= self.form.cleaned_data['password']
-            )
-        else:
-            error = 'Empty username and/or password'
-            return render(request, self.template_name,
-                {'next': next, 'error': error, 'form': self.form}
-            )
-
-        if user is not None:
-            login(request, user)
-            if next is not '':
+        if request.user.is_authenticated:
+            if next != '':
                 return HttpResponseRedirect(next)
             else:
                 return HttpResponseRedirect(reverse('home:index'))
+
+        if page == 'signup':
+            self.form = RegisterForm()
         else:
-            error = 'Invalid username and/or password'
-            return render(request, self.template_name,
-                {'next': next, 'error': error, 'form': self.form}
-            )
+            self.form = LoginForm()
+
+        return render(request, self.template_name,
+            {'next': next, 'form': self.form, 'page': page}
+        )
+
+    def post(self, request, next='', page='login', user=None):
+        if 'next' in request.POST:
+            next = request.POST['next']
+
+        if page == 'signup':
+            self.form = RegisterForm(request.POST)
+            if self.form.is_valid():
+                return HttpResponse('user valid')
+            else:
+                return HttpResponse('user error')
+
+        else:
+            self.form = LoginForm(request.POST)
+            if self.form.is_valid():
+                user = authenticate(username=self.form.cleaned_data['username'],
+                    password= self.form.cleaned_data['password']
+                )
+                if user is not None:
+                    login(request, user)
+                    if next is not '':
+                        return HttpResponseRedirect(next)
+                    else:
+                        return HttpResponseRedirect(reverse('home:index'))
+                else:
+                    self.error_string = "Your username and password didn't match. Please try again."
+
+            return render(request, self.template_name,{
+                'next': next,
+                'form': self.form,
+                'page': page,
+                'error_string': self.error_string
+            })
