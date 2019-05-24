2a82623d39b1582b105a97eef2b331333ead577b
Kioko Kenndey <kmkioko@gmail.com>
2019-05-24 19:10:11 +0300

    python requirements, settings.py ignored

--- letlive/settings_.py
+++ letlive/settings_.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%i$t%2q98p&*5dzkh@nmip2sj%_37t@m0e-iwn-ly9n20=v!zh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djecrety',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
