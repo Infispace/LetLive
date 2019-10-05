633d7e132f1f600af1035e2e6df0befb6d174e07
Kioko Kennedy <kmkioko@gmail.com>
2019-10-05 14:13:52 +0300

    Revert "pip freeze requirements.txt"
    This reverts commit 5a5cf6f5cdf9220b615aa518d230a37403e0adcf.

--- home/views/indexView.py
+++ home/views/indexView.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import AccessMixin, LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_http_methods
+from django.urls import reverse
 from django.views.generic import ListView
-from home.models import Article
 
+from home.models import Article
 
 class IndexView(ListView):
     model = Article
